516141769de8e793c0a32e8a830a2d0116d2a277
Dinacel <admin@dinacel.com>
2023-03-22 00:02:31 +0100

    Create Admin.vue

---
+++ src/views/Admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    Test
+  </v-container>
+</template>
+
+<script>
+//import { useBreadcrumbsStore } from '@/stores/breadcrumbs';
+export default {
+  name: 'home',
+  setup: function() {
+    //const breadcrumbsStore = useBreadcrumbsStore();
+    //breadcrumbsStore.$reset();
+  }
+};
+</script>
