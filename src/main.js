635238b332dc8eed9fab455eadc6c40cefcb8b72
Dinacel <admin@dinacel.com>
2023-03-21 23:52:53 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from '@/App.vue';
+import router from '@/router';
+import pinia from '@/plugins/pinia';
+import idsrvAuth from '@/utils/idsrvAuth';
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+idsrvAuth.startup().then(ok => {
+  if (ok) {
+    new Vue({
+      pinia,
+      router,
+      render: h => h(App)
+    }).$mount('#app');
+  } else {
+    console.log('Startup was not ok');
+  }
+}).catch(function() {
+  console.log('Startup was not ok (idsrvAuth)');
+});
