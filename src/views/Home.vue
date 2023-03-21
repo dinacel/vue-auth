a1929808556e2196fc36e9b26deadbd231e7cc3e
Dinacel <admin@dinacel.com>
2023-03-22 00:01:52 +0100

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    Test
+  </v-container>
+</template>
+
+<script>
+//import { useBreadcrumbsStore } from '@/stores/breadcrumbs';
+
+export default {
+  name: 'home',
+
+  setup: function() {
+    //const breadcrumbsStore = useBreadcrumbsStore();
+    //breadcrumbsStore.$reset();
+  }
+};
+</script>
